9bbf88b3032c1ba17fba28253c47dfd38cda68af
Tropizz <tomivaillant@gmail.com>
2024-12-20 10:28:21 +0100

    update_client

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/tomiv/AppData/Local/JetBrains/CLion 2024.3.1/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/MinGW/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
